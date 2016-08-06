5c0da68d42e76edb475714e4d58cbe750e36f0b2
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-08-06 11:42:57 +0200

    Use multi-channel tick sound play
    Allow the same sound being played simultaneously

--- src/effects.js
+++ src/effects.js
@@ -1,7 +1,18 @@
 const soundOrigin = 'http://soundbible.com/grab.php?id=2108&type=mp3'
 const tickDelay = 200 // ms
-const tick = new Audio(soundOrigin, {loop: false})
+const poolSize = 3
+const tickPool = Array.from(Array(poolSize)).map(() => new Audio(soundOrigin, {loop: false}))
+
+function* gen (n = 0) {
+  yield 0
+  yield 1
+  yield 2
+  yield* gen()
+}
+
+const getNextIndex = gen();
 
 export const tickSound = () => {
-  setTimeout(() => tick.play(), tickDelay)
+  const r = getNextIndex.next().value
+  setTimeout(() => tickPool[r].play(), tickDelay)
 }
