9330fe516931188abecce913d248528af9732896
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-08-06 23:34:42 +0200

    Extract positions

--- src/components/Wire.js
+++ src/components/Wire.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Wire.css'
 import * as colorCodes from '../constants/colors'
+import { LEFT, RIGHT } from '../constants/positions'
 
 const Wire = ({colorNameLeft, colorNameRight, left, right, inc, dec}) => {
 
   const positions = [
-    ...Array.from(Array(left)).map(() => 'left'),
-    ...Array.from(Array(right)).map(() => 'right')
+    ...Array.from(Array(left)).map(() => LEFT),
+    ...Array.from(Array(right)).map(() => RIGHT)
   ]
 
   return (
@@ -18,7 +19,7 @@ const Wire = ({colorNameLeft, colorNameRight, left, right, inc, dec}) => {
           className={"Wire-cell Wire-cell-" + pos}
           onClick={() => { if (pos === 'left') { inc(i) } else { dec(i); } }}
           style={{backgroundColor: colorCodes[(i < (left + right)/2) ? colorNameLeft : colorNameRight]}}
-          />
+        />
       )}
     </div>
   )
