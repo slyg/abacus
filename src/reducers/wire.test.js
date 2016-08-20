86188fea02f15d2a4aa5c62206e2e7412e55c697
Sylvain Faucherand <syl.faucherand@gmail.com>
2016-08-20 23:46:55 +0100

    Fix tests

--- src/reducers/wire.test.js
+++ src/reducers/wire.test.js
@@ -5,18 +5,18 @@ import { expect } from 'chai'
 describe('wire reducer', () => {
   it('should init with a default value', () => {
     expect(
-      reducer(undefined, {type: NOOP})
+      reducer(10)(undefined, {type: NOOP})
     ).to.eql({ left: 10, right: 0, focusIndex: -1 })
   })
   it('should increment left value and decrement on right value on INCREMENT message', () => {
-    const state0 = reducer(undefined, {type: NOOP})
-    const state1 = reducer(state0, {type: INCREMENT, index: 7})
+    const state0 = reducer(10)(undefined, {type: NOOP})
+    const state1 = reducer(10)(state0, {type: INCREMENT, index: 7})
     expect(state1).to.eql({ left: 7, right: 3, focusIndex: -1 })
   })
   it('should decrement left value and increment on right value on DECREMENT message', () => {
-    const state0 = reducer(undefined, {type: NOOP})
-    const state1 = reducer(state0, {type: INCREMENT, index: 0})
-    const state2 = reducer(state1, {type: DECREMENT, index: 3})
+    const state0 = reducer(10)(undefined, {type: NOOP})
+    const state1 = reducer(10)(state0, {type: INCREMENT, index: 0})
+    const state2 = reducer(10)(state1, {type: DECREMENT, index: 3})
     expect(state2).to.eql({ left: 4, right: 6, focusIndex: -1 })
   })
 })
